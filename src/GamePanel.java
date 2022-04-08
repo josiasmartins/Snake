900183efde81b0ae5d7770e889df0129991a39d0
Caitano <josiasmartinscaitano@gmail.com>
2022-04-08 17:26:30 -0300

    fixture(GamePanel): adicionado o scopo dos métodos

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,15 +1,53 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
-import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     GamePanel() {
 
-    }
+    };
+
+    public void startGame() {
+
+    };
+
+    public void paintComponent(Graphics g) {
+
+    };
+
+    public void drawer(Graphics g) {
+
+    };
+
+    public void move() {
+
+    };
+
+    public void checkApple() {
+
+    };
+
+    public void checkCollisions() {
+
+    };
+
+    public void gameOver(Graphics g) {
+
+    };
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    };
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
